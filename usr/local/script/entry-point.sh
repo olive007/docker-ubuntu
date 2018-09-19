466f995cd4402d5ef105a686e80773f051510365
Olivier SECRET <olivier.secret@catalyst-au.net>
2018-09-19 16:54:50 +1000

    Fix entry-point bug

--- usr/local/script/entry-point.sh
+++ usr/local/script/entry-point.sh
@@ -5,13 +5,15 @@ set -e
 if [ -t 0 ] ; then
 
     echo "Container started with interactive shell"
-    # Start bash command
+    # Execute command from CMD value
     /bin/bash -c "$@"
 
 else
 
     echo "Container started without interactive shell"
-    # Start an infinite loop
+    # Execute command from CMD value
+    /bin/bash -c "$@"
+    # Start an infinite loop after to don't stop the container
     while true
     do
 	sleep 60
